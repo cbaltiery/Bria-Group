ecf15ae896499710a2ac4215e4579c36dfce22b3
Tyler Wingood <110292065+T-Wingood@users.noreply.github.com>
2022-10-02 18:44:15 -0400

    Added Member Garden Page, and bootstrap connection

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MemberGarden from './components/MemberGarden';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+  <>
+  <h1> Hello from App.jsx</h1>
+  </>
+  );
+}
+
+export default App;
